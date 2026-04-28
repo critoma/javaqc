34966afe64bb8a196998de2bc8222557f108627e
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2026-04-28 14:41:37 +0300

    feat(module): add jdk.httpserver dependency and render package export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.redfx.strange {
     requires java.logging;
+    requires jdk.httpserver;
 
     exports org.redfx.strange;
     exports org.redfx.strange.algorithm;
     exports org.redfx.strange.gate;
     exports org.redfx.strange.local;
+    exports org.redfx.strange.render;
 }
